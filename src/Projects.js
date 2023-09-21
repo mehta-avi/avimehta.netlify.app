38d711ca69b47a663d0cd9ad9cf6931fbdf7863c
Avi <avimehta9@gmail.com>
2023-09-20 20:22:45 -0400

    added new stuff

--- src/Projects.js
+++ src/Projects.js
@@ -5,41 +5,55 @@ const Projects = () => {
         {
           title: 'MoodSync',
           short_description: 'An emotion-based AI spotify music player',
+          long_description: '',
           imageUrl: 'logos/moodsync.jpg',
           projectURL: 'https://github.com/mehta-avi/MoodSync',
-        }, 
+        },
+        {
+          title: 'WebNotes',
+          short_description: 'A private journal hosted on the Internet',
+          long_description: '',
+          imageUrl: 'logos/WebNotes.jpg',
+          projectURL: 'https://github.com/mehta-avi/WebNotes',
+        },  
         {
           title: 'ChefMeUp',
           short_description: "A mobile app which tackles food-insecurity by finding affordable groceries and recipes based on the user's zip code",
+          long_description: '',
           imageUrl: 'logos/ChefMeUp.jpg',
           projectURL: 'https://github.com/Aeonss/ChefMeUp',
         },
         {
           title: 'Manajira',
           short_description: 'An agile project management workflow organizer',
+          long_description: '',
           imageUrl: 'logos/manajira.jpg',
           projectURL: 'https://github.com/mehta-avi/projectManager',
         },
         {
           title: 'FindMeParking',
           short_description: "An AI-based parking recommender factoring in the user's current location",
+          long_description: '',
           imageUrl: 'logos/FMP.jpg',
           projectURL: 'https://github.com/avocados23/capstone-spring2023',
         },
         {
           title: 'ShortLR',
           short_description: "A URL shortener",
+          long_description: '',
           imageUrl: 'logos/shortlr.jpg',
           projectURL: 'https://github.com/mehta-avi/ShortLR'
         },
         {
           title: 'Custom Memory Manager',
           short_description: 'A custom multi-threaded dynamic storage allocator. Code available upon request',
+          long_description: '',
           imageUrl: 'logos/malloc.jpg',
         },
         {
           title: 'Unix Shell',
           short_description: 'A custom multi-functional unix shell. Code available upon request',
+          long_description: '',
           imageUrl: 'logos/cush.jpg',
         },
       ];    
